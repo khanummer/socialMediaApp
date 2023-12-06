68f59c23e71e821b5ac904775569c702aecbb420
khanummer <ummernkhan@gmail.com>
2023-12-05 22:40:50 -0800

    adds ejs, edits server.js to have express, app.get, and app.listen

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(PORT, () => {
+    console.log('listening on port 3000')
+});
