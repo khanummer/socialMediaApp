c8738a8294014396bf36113d84f3127556a91e4b
khanummer <ummernkhan@gmail.com>
2023-12-06 13:02:44 -0800

    adds public folder with css, imgs. adds app.use public folder in server.js, connects the stylesheet through index.ejs

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # socialMediaApp
+Meet 'Nothing Social' The Social Media App styled around the 'Nothing' Tech Company ran by Carl Pei
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Using 'public' folder
+app.use(express.static(__dirname + "/public"));
 
+// Get Home Page
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
