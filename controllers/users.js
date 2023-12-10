2a26610c6c7b3447b8884446866adbb387bb733e
khanummer <ummernkhan@gmail.com>
2023-12-10 15:14:00 -0800

    adds post route in users.js controller to post a new user to the mongo database

--- controllers/users.js
+++ controllers/users.js
@@ -4,11 +4,30 @@ const router = express.Router();
 const User = require('../models/users');
 
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
 
+// new get route
 router.get('/new', (req, res) => {
-    res.render('./users/new.ejs')
+    res.render('./users/new')
 });
+
+// new post route
+router.post('/new', (req, res) => {
+
+    // setting the request form info to variables
+    const username = req.body.username;
+    const password = req.body.password;
+    const bio = req.body.bio;
+
+    // entering into the database
+    const newUser = {};
+    newUser.username = username;
+    newUser.password = password;
+    newUser.bio = bio;
+
+    // console.log('THIS IS NEW USER ' + newUser);
+    console.log(newUser);
+    res.redirect('/');
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
+const bodyParser = require('body-parser');
 const PORT = 3000;
 const userController = require('./controllers/users');
 
@@ -10,8 +11,11 @@ require('./db/db');
 // Using 'public' folder
 app.use(express.static(__dirname + "/public"));
 
+// using bodyParser to parse the body and get access to req.body
+app.use(bodyParser.urlencoded({extended: false}));
 
-// // ?
+
+// setting default view engine and extension 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
@@ -21,13 +25,10 @@ app.use('/users', userController);
 
 // Get Home Page
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index');
 });
 
-// Get Login Page
-// app.get('/signup', (req, res) => {
-//     res.render('/users/new')
-// });
+
 
 app.listen(PORT, () => {
     console.log('listening on port 3000')
