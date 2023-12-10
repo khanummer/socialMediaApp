f996ad13888e61426403b271effa444d0db2a86a
khanummer <ummernkhan@gmail.com>
2023-12-09 23:53:49 -0800

    fixes bug where sign up page would not be redirected from app.get. adds users.js in controllers and /users/new.ejs in views.

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,14 @@
 const express = require('express');
+const app = express();
+const router = express.Router();
 const User = require('../models/users');
+
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+router.get('/new', (req, res) => {
+    res.render('./users/new.ejs')
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const PORT = 3000;
+const userController = require('./controllers/users');
 
 // require mongoose database
 require('./db/db');
@@ -10,20 +11,23 @@ require('./db/db');
 app.use(express.static(__dirname + "/public"));
 
 
-// ?
+// // ?
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
+// Using user router 
+app.use('/users', userController);
 
 
 // Get Home Page
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index.ejs');
 });
 
 // Get Login Page
-app.get('/signup', (req, res) => {
-    res.render('/users/new')
-});
+// app.get('/signup', (req, res) => {
+//     res.render('/users/new')
+// });
 
 app.listen(PORT, () => {
     console.log('listening on port 3000')
